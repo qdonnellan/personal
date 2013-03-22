f0f14d02315bebbf37b1541bd5d43f92096f511e
qdonnellan <qdonnellan@gmail.com>
2013-03-21 20:27:55 -0500

    edits to styling, especially footer, blog page, and front page

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
+# Copyright (C) 2013 Quentin Donnellan
+# http://qdonnellan.appspot.com
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 import webapp2
 from handlers import MainHandler
 from google.appengine.api import users
@@ -14,6 +36,13 @@ class BlogPage(MainHandler):
       blog_active = "active", 
       admin = users.is_current_user_admin(),
       blog_content = formatContent(database.getBlog(get_all = True)))
+
+class BlogPermalink(MainHandler):
+  def get(self, postID):
+    self.render('blog.html', 
+      single_post = True, 
+      admin = users.is_current_user_admin(),
+      blog_content = formatContent([database.getBlog(contentID = postID)]))
 
 class AuthPage(MainHandler):
   def get(self):
@@ -36,6 +65,7 @@ class EditBlog(MainHandler):
 
 app = webapp2.WSGIApplication([
   ('/blog', BlogPage),
+  ('/blog/(\w+)', BlogPermalink),
   ('/auth', AuthPage),
   ('/edit_blog', EditBlog),
   ('/edit_blog/(\w+)', EditBlog),
