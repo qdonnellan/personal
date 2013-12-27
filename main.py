482b3c6a3cbb62b3087ae616906311bcf191c573
qdonnellan <qdonnellan@gmail.com>
2013-12-26 19:00:13 -0600

    updated handlers for single blog page

--- main.py
+++ main.py
@@ -6,19 +6,29 @@ import webapp2
 from handlers import MainHandler
 from blog import blogAPI
 import json
+import logging
 
 class MainPage(MainHandler):
   def get(self):
     self.render('home.html', home_active ="active")
 
 class BlogPage(MainHandler):
-  def get(self):
-    blogJSON = json.loads(blogAPI().get_latest())
-    self.render('blog.html', home_active ="active", page_name = "blog", blogJSON = blogJSON)
+  def get(self, year=None, month=None, day=None):
+    blog = blogAPI()
+    if not any([year, month, day]):
+      blogJSON = blog.get_latest()
+    else:
+      blogJSON = blog.fetch_blog_post(year, month, day)
+    self.render('blog.html', 
+      home_active ="active", 
+      page_name = "blog",
+      blogJSON = json.loads(blogJSON),
+      blogMAP = json.loads(blog.get_map()))
 
 app = webapp2.WSGIApplication([
   ('/api/blog/(\w+)/(\w+)/(\w+)', blogAPI),
   ('/api/blog/(\w+)', blogAPI),
+  ('/blog/(\w+)/(\w+)/(\w+)', BlogPage),
   ('/blog', BlogPage),
   ('.*', MainPage)
   ],debug=True)
